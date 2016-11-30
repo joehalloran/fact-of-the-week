c72cabd306352ebd7c5f84e5befe643d8f062c38
joehalloran <joe_halloran@hotmail.co.uk>
2016-11-30 22:10:44 +0000

    Production settings2

--- factoftheweek/config/settings/production.py
+++ factoftheweek/config/settings/production.py
@@ -10,3 +10,13 @@ import dj_database_url
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
+
+# Static file
+
+STATIC_ROOT = os.path.join(BAS_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BAS_DIR, 'static'),
+)
