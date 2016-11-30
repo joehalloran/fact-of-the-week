dc8f71284dcc2c8f2637018f90e8f97f8e853715
joehalloran <joe_halloran@hotmail.co.uk>
2016-11-30 22:07:07 +0000

    Production settings

--- factoftheweek/config/settings/production.py
+++ factoftheweek/config/settings/production.py
@@ -1,7 +1,12 @@
-# settings/dev.py
+# settings/production.py
 from .base import *
 
 DEBUG = FALSE
 
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
